ed9fda95ae3db24df70d908c58a5406ef25e6b89
imarcho <imarcho@yandex.ru>
2026-05-03 19:46:11 +0300

    ewviinb

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Довавь свою первуб задачу!</p>
+                <p className="text-center">Создай свою первую задачу!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
@@ -96,7 +96,7 @@ function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={e => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="Добавь задачу"
+                    placeholder="Напиши задачу"
                     aria-describedby="basic-addon1"
                 />
                 <InputGroup.Append>
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Добавить задачу'}
+                        {submitting ? 'Adding...' : 'Добавить'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
