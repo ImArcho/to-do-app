456ae6f7973b6ac1d43ed38d4d6671fddce78155
imarcho <imarcho@yandex.ru>
2026-05-04 16:42:44 +0300

    test-1777902164

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add item'}
+                        {submitting ? 'Adding...' : 'Добавить'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
