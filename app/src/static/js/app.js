570ba1f9fd7ce770241ce88d4dca61360fac5237
imarcho <imarcho@yandex.ru>
2026-05-04 23:44:04 +0300

    Revert "test-1777902164"
    This reverts commit 456ae6f7973b6ac1d43ed38d4d6671fddce78155.

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Добавить'}
+                        {submitting ? 'Adding...' : 'Add item'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
