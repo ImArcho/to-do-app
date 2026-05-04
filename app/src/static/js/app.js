422363f4336225578ff0990efb35dee0cead3eb5
imarcho <imarcho@yandex.ru>
2026-05-04 16:27:24 +0300

    test-1777901244

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -96,7 +96,7 @@ function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={e => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="New item"
+                    placeholder="Новая задача"
                     aria-describedby="basic-addon1"
                 />
                 <InputGroup.Append>
