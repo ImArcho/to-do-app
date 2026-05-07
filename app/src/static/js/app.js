e19255161bf8eb2f0a3540dbf4364d3734d3ec21
imarcho <imarcho@yandex.ru>
2026-05-07 14:51:21 +0300

    Ru the app

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">Начните планировать свой день!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
@@ -96,7 +96,7 @@ function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={e => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="New Item"
+                    placeholder="Написать.."
                     aria-describedby="basic-addon1"
                 />
                 <InputGroup.Append>
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Добавить'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
@@ -166,7 +166,7 @@ function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
                         size="sm"
                         variant="link"
                         onClick={removeItem}
-                        aria-label="Remove Item"
+                        aria-label="Удалить"
                     >
                         <i className="fa fa-trash text-danger" />
                     </Button>
