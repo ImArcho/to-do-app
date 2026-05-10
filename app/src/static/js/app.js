039f15d8d66a7f558c52d03b1bcfcb7b319ce289
imarcho <imarcho@yandex.ru>
2026-05-10 15:14:58 +0300

    Test_1.3

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Привет, как дела?</p>
+                <p className="text-center">Я уже не знаю что сюда писать.</p>
             )}
             {items.map(item => (
                 <ItemDisplay
@@ -96,7 +96,7 @@ function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={e => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="Написать"
+                    placeholder="Поле для текста"
                     aria-describedby="basic-addon1"
                 />
                 <InputGroup.Append>
